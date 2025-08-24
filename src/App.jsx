a4ad18b4bb532c6a97911464e7a2eab0388247c8
practicegit080825 <practicegithub080825@gmail.com>
2025-08-24 13:03:26 +0530

    Complete-video-05: Project 1: Setup + Add Bootstrap to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
 import "./App.css";
 
-let name = "Arpit Katiyar"
+let name = "Arpit Katiyar";
 function App() {
   return (
     <>
-      <nav>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Textutils
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
       </nav>
-      <h1>Namaskar {name},</h1>
-      <div className="container">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptas quos tempore nemo incidunt omnis neque, illo quam impedit voluptatum recusandae numquam aperiam quasi quisquam modi error dolores aut tempora sapiente! At laborum perspiciatis, beatae facere dolore repudiandae?
-      </div>
     </>
   );
 }
