eae3f6347898916abb52fbe9603e99c73c8b9045
practicegit080825 <practicegithub080825@gmail.com>
2025-08-25 13:14:17 +0530

    Complete-video-10: Create 'Enable Dark Mode' Button, Use useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -8,7 +9,8 @@ function App() {
     <>
       <Navbar title="Textutils" />
       <div className="container my-3">
-        <TextForm heading="Enter Your Text Here:"/>
+        {/* <TextForm heading="Enter Your Text Here:"/> */}
+        <About />
       </div>
     </>
   );
